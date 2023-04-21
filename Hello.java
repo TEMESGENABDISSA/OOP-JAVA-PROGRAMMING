9359ac7a6450533055e6a24d901ab53f2c37921b
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-04-21 10:43:11 +0300

    well come to java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4 ;
+        int sum = 0;
+        sum = a + b;
+     System.out.println( " temesgen abdissa");
+     System.out.println( " your sum is " + sum);
+        
+    }
+}
