fb4522143b6857ea76ec7050ed65ed335b37b1b0
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-04-21 11:07:04 +0300

    print result

---
+++ PrintResult.java
@@ -0,0 +1,10 @@
+public class PrintResult {
+
+    public static void main ( String [] args){
+        int a = -5+8*6;
+        int b = ( 55+9)%9;
+        System.out.println(" print the first result is " + a);
+        System.out.println(" print the second result is " +b);
+    }
+}
+
