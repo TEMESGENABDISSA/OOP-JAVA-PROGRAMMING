69338fb5bcb5eab041dbb8d10bd13cd9952eb9e2
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-04-21 10:44:33 +0300

    well come to java

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
 public class Hello{
     public static void main(String[] args) {
-        int a = 2;
-        int b = 4 ;
-        int sum = 0;
-        sum = a + b;
-     System.out.println( " temesgen abdissa");
-     System.out.println( " your sum is " + sum);
+    
+     System.out.println( " well come  to  object oriented programming language");
+     System.out.println( "  coding is fun with java");
         
     }
 }
