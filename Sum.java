be5e6efd2cf929fcf73ff6b2993a4af2a945fefd
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-04-21 11:08:39 +0300

    find sum

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    public static void main(String[] args){
+        int num1= 4;
+        int num2 = 6;
+        int sum = 0;
+        sum = num1 + num2 ;
+        System.out.println(" Your sum is " + sum  );
+
+
+    }
+}
